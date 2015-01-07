a65514e10cf3cfbe1b3a212cf010d64ad15725fa
Wee Keat Chin <weekeat@visualdensity.com>
2015-01-08 09:05:43 +1100

    More refactoring, Twig templates layouts

--- src/Controllers/DefaultController.php
+++ src/Controllers/DefaultController.php
@@ -2,7 +2,10 @@
 $default = $app['controllers_factory'];
 
 $default->get('/', function() use ($app) {
-    return $app->redirect('/mylink');
+    return $app['twig']->render('Default/index.html.twig', array(
+        'name' => 'Mamamia',
+    ));
+
 });
 
 return $default;
--- src/Controllers/MyController.php
+++ src/Controllers/MyController.php
@@ -5,7 +5,6 @@ $my->get('/', function() use ($app) {
     return $app['twig']->render('hello.html.twig', array(
         'name' => 'Mamamia',
     ));
-    //return 'My controller content';
 });
 
 return $my;
